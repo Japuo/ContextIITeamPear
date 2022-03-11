58931b1a921433aa44b499406cb5a62e715e2e09
Jacco Verhoeckx <jaccozakelijk@hotmail.com>
2022-03-11 11:00:53 +0100

    Added some camera fixes and a GameManager

--- SustainabilityGame/Assets/Scripts/CameraManager.cs
+++ SustainabilityGame/Assets/Scripts/CameraManager.cs
@@ -5,22 +5,25 @@ using Cinemachine;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] private Camera myCamera = null;
-    [SerializeField] private CinemachineVirtualCamera myCineMachine = null;
+    [SerializeField] private Camera mainCamera = null;
     [SerializeField] private GameObject followPointer = null;
-
     [SerializeField] private GameObject player = null;
+
 
     Vector3 mousePosition = new Vector3();
     Vector3 followPointerPosition = new Vector3();
+
     void Update()
     {
-        mousePosition = myCamera.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0f;
 
         followPointerPosition = (mousePosition + player.transform.position) / 2; //midpoint calculation
         followPointerPosition = (followPointerPosition + player.transform.position) / 2; //quarterpoint calculation
+
         followPointer.transform.position = followPointerPosition;
 
+
     }
+
 }
---
+++ SustainabilityGame/Assets/Scripts/Interactible.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactible : MonoBehaviour
+{
+    public UnityEvent onInteract;
+    public GameObject icon;
+    public GameObject cameraAncor;
+
+    bool isPlayerInRange;
+    public bool isInteractive = true;
+
+    private void Start()
+    {
+        isPlayerInRange = false;
+        if(!CompareTag("Interactable")) { Debug.LogWarning("Interactible " + name + " does not have Interactible tag. This might or might not break things."); }
+        if(gameObject.layer != 3) { Debug.LogWarning("Interactible " + name + " does not have Interactible layer. This might or might not break things."); }
+
+    }
+
+    private void Update()
+    {
+        if(isPlayerInRange)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                onInteract.Invoke();
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && isInteractive)
+        {
+            isPlayerInRange = true;
+            icon.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            icon.SetActive(false);
+        }
+    }
+
+    public void SetInteractible(bool _activated)
+    {
+        isInteractive = _activated;
+        icon.SetActive(_activated && isPlayerInRange);
+    }
+}
