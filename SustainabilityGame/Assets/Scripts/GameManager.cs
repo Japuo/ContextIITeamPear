f548f296a93f825738135182c74078a225452bbb
Mana <manakouwenberg@gmail.com>
2022-03-31 15:19:29 +0200

    Introduction text added, fixed credits being on in the Main Menu

--- SustainabilityGame/Assets/Scripts/GameManager.cs
+++ SustainabilityGame/Assets/Scripts/GameManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject player;
+    public UnityEvent startEvents;
 
-    public void TeleportPlayer(GameObject target)
+    private void Start()
     {
-        player.transform.position = target.transform.position;
+        startEvents.Invoke();
     }
 }
